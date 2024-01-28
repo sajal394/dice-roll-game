77d2044ea5ceaa7eabc7d470a09d60e5198fc1b4
sajal2003 <23f2002548@ds.study.iitm.ac.in>
2024-01-29 01:21:17 +0530

    fix

--- script.js
+++ script.js
@@ -79,7 +79,7 @@ btnHold.addEventListener('click', function () {
       scores[activePlayer];
 
     // 2. check if player's score is <=100
-    if (scores[activePlayer] >= 10) {
+    if (scores[activePlayer] >= 30) {
       playing = false;
       diceEl.classList.add('hidden');
 
